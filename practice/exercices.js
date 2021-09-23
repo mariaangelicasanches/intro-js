092c8117823024fca96a321b5b6faf481fcebbc1
Maria Angelica Sanches <mariaangelicasanches@outlook.com>
2021-09-22 23:06:38 -0400

    updated the files

---
+++ practice/exercices.js
@@ -0,0 +1,46 @@
+// array exercices
+
+// function findFruitIndex(arr, item) {
+//     //indexOf
+//     let indexOfFruit = arr.indexOf(item)
+//     return indexOfFruit
+// }
+// let array = ['mango', 'dorian', 'loquat', 'jackfruit', 'spanish lime', 'water melon', 'cotton candy grapes', 123];
+// const fruit = 'dorian'
+
+// console.log('The index of', fruit, 'is', findFruitIndex(array, fruit))
+
+
+
+
+// let array = ['mango', 'dorian', 'loquat', 'jackfruit', 'spanish lime', 'water melon', 'cotton candy grapes', 123];
+// console.log(array.pop())
+// console.log(array)
+
+// console.log(array.unshift(123))
+// console.log(array)
+
+ 
+
+// const fruit = 'water melon'
+
+/*let indexOfFruit = arr[0]
+
+function findFruitIndex(arr, item) {
+    for (let i = 0;  i < arr.length; i++) {
+    console.log(`Index of fruit is: ${})
+    }   
+}*/
+
+
+let str = 'Manny is happy, and Happy Birth Manny'
+
+let replace = str.replace('happy', 'content')
+
+//console.log(replace)
+
+
+let arrOfWords = str.split(' ')
+
+console.log(arrOfWords)
+
